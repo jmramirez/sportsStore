04adbdc5b65602cd4272762e5e7f0742506d6cd0
Jose M Ramirez <josemramirez81@outlook.com>
2019-02-15 16:24:48 -0500

    First Commit

---
+++ SportsStore/App_Start/IdentityConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using SportsStore.Infrastructure.Identity;
+
+[assembly: OwinStartup(typeof(SportsStore.IdentityConfig))]
+namespace SportsStore
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app) {
+            app.CreatePerOwinContext<StoreIdentityDbContext>(StoreIdentityDbContext.Create);
+            app.CreatePerOwinContext<StoreUserManager>(StoreUserManager.Create);
+            app.CreatePerOwinContext<StoreRoleManager>(StoreRoleManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions { AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie });
+
+        }
+    }
+}
---
+++ SportsStore/Models/ProductDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext() : base("SportStoreDb")
+        {
+            Database.SetInitializer<ProductDbContext>(new ProductDbInitializer());
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+    }
+}
